7ca6714525ee0d300a52401aeeb2a9e0253b0c6f
jesternotjoker <vanovikov5@yandex.ru>
2022-03-15 18:27:00 +0300

    2lab

---
+++ Lab1.2/data_analytics_lab.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot
+from openpyxl import load_workbook
+
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+def getvalue(x): return x.value
+
+year = list(map(getvalue, sheet['A'][1:]))
+temp = list(map(getvalue, sheet['C'][1:]))
+sun = list(map(getvalue, sheet['D'][1:]))
+
+pyplot.plot(year, temp, label='Температура, С')
+pyplot.plot(year, sun, label='Активность Солнца')
+pyplot.grid(True)
+pyplot.title('График зависимости изменения температуры от активности Солнца')
+pyplot.show()
+
